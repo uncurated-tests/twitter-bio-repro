243b28cb6543d7b0d21efab9e752e2b5cf6ae9da
Steven Tey <stevensteel97@gmail.com>
2023-03-29 02:46:34 -0500

    repro

--- utils/OpenAIStream.ts
+++ utils/OpenAIStream.ts
@@ -3,6 +3,7 @@ import {
   ParsedEvent,
   ReconnectInterval,
 } from "eventsource-parser";
+import { NextFetchEvent, NextRequest } from "next/server";
 
 export type ChatGPTAgent = "user" | "system";
 
@@ -23,11 +24,14 @@ export interface OpenAIStreamPayload {
   n: number;
 }
 
-export async function OpenAIStream(payload: OpenAIStreamPayload) {
+export async function OpenAIStream(
+  payload: OpenAIStreamPayload,
+  domains: string[] = [],
+  req: NextRequest,
+  ev: NextFetchEvent
+) {
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
-
-  let counter = 0;
 
   const res = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: {
@@ -38,43 +42,48 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
     body: JSON.stringify(payload),
   });
 
-  const stream = new ReadableStream({
-    async start(controller) {
-      // callback
-      function onParse(event: ParsedEvent | ReconnectInterval) {
-        if (event.type === "event") {
-          const data = event.data;
-          // https://beta.openai.com/docs/api-reference/completions/create#completions/create-stream
-          if (data === "[DONE]") {
-            controller.close();
-            return;
-          }
-          try {
-            const json = JSON.parse(data);
-            const text = json.choices[0].delta?.content || "";
-            if (counter < 2 && (text.match(/\n/) || []).length) {
-              // this is a prefix character (i.e., "\n\n"), do nothing
+  let fullResponse = "";
+
+  const readableStream = res.body;
+
+  const transformStream = new TransformStream({
+    async transform(chunk, controller) {
+      const parser = createParser(
+        async (event: ParsedEvent | ReconnectInterval) => {
+          if (event.type === "event") {
+            const data = event.data;
+
+            if (data === "[DONE]") {
+              console.log("done, signaling end of stream...");
+              controller.terminate(); // terminate transformstream
               return;
             }
-            const queue = encoder.encode(text);
-            controller.enqueue(queue);
-            counter++;
-          } catch (e) {
-            // maybe parse error
-            controller.error(e);
+
+            try {
+              const json = JSON.parse(data);
+              const text = json.choices[0].delta?.content || "";
+              fullResponse += text;
+              if (fullResponse.length > 20) {
+                // simulate async process
+                const transformedResponse = await new Promise((resolve) =>
+                  setTimeout(() => resolve(fullResponse.toUpperCase()), 1000)
+                );
+                console.log("transformed response: ", transformedResponse);
+                controller.enqueue(encoder.encode(transformedResponse + "\n"));
+                fullResponse = "";
+              }
+            } catch (e) {
+              // maybe parse error
+              controller.error(e);
+            }
           }
         }
-      }
+      );
 
-      // stream response (SSE) from OpenAI may be fragmented into multiple chunks
-      // this ensures we properly read chunks and invoke an event for each SSE event stream
-      const parser = createParser(onParse);
-      // https://web.dev/streams/#asynchronous-iteration
-      for await (const chunk of res.body as any) {
-        parser.feed(decoder.decode(chunk));
-      }
+      // feed the chunk to the parser
+      parser.feed(decoder.decode(chunk));
     },
   });
 
-  return stream;
+  return readableStream?.pipeThrough(transformStream);
 }
